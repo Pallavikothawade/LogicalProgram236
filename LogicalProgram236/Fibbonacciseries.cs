3a861ed873823856db81e6312c813adcde466ed0
Pallavikothawade <Pallavijkothawade@gmail.com>
2023-01-10 15:59:14 +0530

    Adding fibbo program

---
+++ LogicalProgram236/Fibbonacciseries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram236
+{
+    class Fibbonacciseries
+    {
+        public void fibo()
+        {
+            int var1 = 0, var2 = 1, var3;
+            Console.WriteLine("Enter The Number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(var1 + " ");
+            Console.WriteLine(var2 + " ");
+            for (int i = 2; i < n; ++i)
+            {
+                var3 = var1 + var2;
+                Console.WriteLine(var3 + " ");
+                var1 = var2;
+                var2 = var3;
+            }
+        }
+    }
+}
---
+++ LogicalProgram236/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram236
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n*****************Welcome The Logical Program**********************");
+            Console.WriteLine("Plz Choose Any One Option In Below Program");
+            Console.WriteLine("1:Fibbonacci Series\n2:Perfect Number\n3:Prime Number\n4:Reverse Number\n5:Coupon Number\n6:Stopwatch Program\n");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:Fibbonacciseries fibbonacciseries = new Fibbonacciseries();
+                    fibbonacciseries.fibo();
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
